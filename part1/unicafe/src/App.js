09af87f17c35526575da6166d20f2c918eae52ef
MITHUN TR <tr7mithun@gmail.com>
2023-06-13 18:38:42 +0530

    excercise 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -8,14 +8,6 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const average = (good, neutral, bad) => {
-    const total = good + neutral + bad
-    return (good * 1 + neutral * 0 + bad * (-1))/total
-  }
-  const positive = (good, neutral, bad)=>{
-    const total = good + neutral + bad
-    return `${good * 100 / total}% `
-  }
 
   return (
     <div>
@@ -26,18 +18,40 @@ const App = () => {
       <button onClick={() => setNeutral(neutral + 1)}>nuetral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
 
+      <Statistics good={good} neutral={neutral} bad={bad} />
+
+    </div>
+  )
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+
+  const average = (good, neutral, bad) => {
+    const total = good + neutral + bad
+    return (good * 1 + neutral * 0 + bad * (-1)) / total
+  }
+  const positive = (good, neutral, bad) => {
+    const total = good + neutral + bad
+    return `${good * 100 / total}%`
+  }
+
+  return (
+    <>
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
       <p>all {good + neutral + bad}</p>
-      <p>average {good+neutral+bad && average(good, neutral, bad)}</p>
+      <p>average {good + neutral + bad && average(good, neutral, bad)}</p>
       <p>positive {(good + neutral + bad) && positive(good, neutral, bad)}</p>
-    </div>
+    </>
+
+
+
   )
+
+
 }
-
-
 
 
 
